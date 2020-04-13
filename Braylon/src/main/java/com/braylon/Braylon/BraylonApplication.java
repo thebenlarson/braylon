fc775e0962e0d706fca09c4ee9ca7cd48540a697
Ben Larson <benlarson_fullstack@yahoo.com>
2020-04-13 15:49:07 -0500

    initial commit.

---
+++ Braylon/src/main/java/com/braylon/Braylon/BraylonApplication.java
@@ -0,0 +1,13 @@
+package com.braylon.Braylon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BraylonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BraylonApplication.class, args);
+	}
+
+}
---
+++ Braylon/src/test/java/com/braylon/Braylon/BraylonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.braylon.Braylon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BraylonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
