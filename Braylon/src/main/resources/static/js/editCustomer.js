8ad2ccdaac0042306db4f4ece38f6d01c506a80d
Derek McCool <mccool.derek@yahoo.com>
2020-04-16 11:13:45 -0500

    changes to the editCustomer.js

--- Braylon/src/main/resources/static/js/editCustomer.js
+++ Braylon/src/main/resources/static/js/editCustomer.js
@@ -4,14 +4,17 @@ $(document).ready(function() {
         $('#new-customer-form :input').prop('disabled', false);
         $('#update-customer-button').show();
         $('#reset-customer-button').show();
+        $('#edit-customer-button').hide();
     });
 
     $('#reset-customer-button').click(function(){
         $('#reset-customer-button').hide();
         $('#update-customer-button').hide();
         $('#new-customer-form :input').prop('disabled', true);
-
-
+        $('#edit-customer-button').show();
     });
 
+    $('#update-customer-button').click(function(){
+        $('#edit-customer-button').show();
+    })
 });
