46fd82637694957324265954d27589b24d5f2678
Derek McCool <mccool.derek@yahoo.com>
2020-04-17 12:45:31 -0500

    fixed edit page button issues

--- Braylon/src/main/resources/static/js/editCustomer.js
+++ Braylon/src/main/resources/static/js/editCustomer.js
@@ -3,18 +3,15 @@ $(document).ready(function() {
     $('#edit-customer-button').click(function(){
         $('#edit-customer-form :input').prop('disabled', false);
         $('#update-customer-button').show();
-        $('#reset-customer-button').show();
+        $('#cancel-edit-button').show();
         $('#edit-customer-button').hide();
     });
 
-    $('#reset-customer-button').click(function(){
-        $('#reset-customer-button').hide();
+    $('#cancel-edit-button').click(function(){
+        $('#cancel-edit-button').hide();
         $('#update-customer-button').hide();
         $('#edit-customer-form :input').prop('disabled', true);
         $('#edit-customer-button').show();
     });
 
-    $('#update-customer-button').click(function(){
-        $('#edit-customer-button').show();
-    })
 });
