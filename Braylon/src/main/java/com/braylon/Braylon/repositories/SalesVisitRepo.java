a00e6235ec3cdfd1f3e4529eced217e47a7471f0
Ben Larson <benlarson_fullstack@yahoo.com>
2020-04-17 10:06:28 -0500

    sales performance rough draft

--- Braylon/src/main/java/com/braylon/Braylon/repositories/SalesVisitRepo.java
+++ Braylon/src/main/java/com/braylon/Braylon/repositories/SalesVisitRepo.java
@@ -6,10 +6,12 @@
 package com.braylon.Braylon.repositories;
 
 import com.braylon.Braylon.entities.SalesVisit;
+import com.braylon.Braylon.entities.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SalesVisitRepo extends JpaRepository<SalesVisit, Integer> {
-    
+    List<SalesVisit> findAllByUser(User user);
 }
